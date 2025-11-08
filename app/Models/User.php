26e3ab37ddc1809ea928badb7cbe4b5a1039f254
ssarailoo <ssarailoo@yahoo.com>
2025-11-08 23:20:41 +0330

    feat: add projects relationship to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -41,5 +42,8 @@ class User extends Authenticatable
             'skills' => 'array',
         ];
     }
-
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
