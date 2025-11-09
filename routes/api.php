0d56832df5e5b0025847853054f2b2a8de9114ec
ssarailoo <ssarailoo@yahoo.com>
2025-11-09 21:31:14 +0330

    feat: add api route for project index endpoint

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\ProjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('projects')->controller(ProjectController::class)->group(function () {
+        Route::get('/', 'index');
+    });
+
+});
