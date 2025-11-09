688721ad31bff9318b99736eb498a80b2bc58d78
ssarailoo <ssarailoo@yahoo.com>
2025-11-10 00:11:54 +0330

    feat: add full CRUD endpoints for task with route model binding

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,7 @@ Route::prefix('v1')->group(function () {
         Route::get('/', 'index');
         Route::post('/', 'store');
     });
+    Route::apiResource('tasks', TaskController::class);
+
 
 });
