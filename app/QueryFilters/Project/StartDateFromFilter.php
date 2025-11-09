291a3b38de6ae4fa961f2d2dd28e25ddf902695a
ssarailoo <ssarailoo@yahoo.com>
2025-11-09 20:57:07 +0330

    feat: add start date from filter

---
+++ app/QueryFilters/Project/StartDateFromFilter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\QueryFilters\Project;
+
+use App\QueryFilters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class StartDateFromFilter extends Filter
+{
+    protected function apply(Builder $query, $value)
+    {
+        return $query->where('start_date', '>=', $value);
+    }
+
+    protected function filterName(): string
+    {
+        return 'start_date_from';
+    }
+}
