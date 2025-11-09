74811eafb0ec345128e09d3ab976dcae084845c6
ssarailoo <ssarailoo@yahoo.com>
2025-11-09 22:29:34 +0330

    feat: add store endpoint

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\IndexProjectRequest;
+use App\Http\Requests\StoreProjectRequest;
 use App\Http\Resources\ProjectResource;
 use App\Services\ProjectService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProjectController extends Controller
 {
@@ -33,9 +34,17 @@ class ProjectController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request): JsonResponse
     {
-        //
+        try {
+            $project = $this->projectService->createProject($request->validated());
+            return response()->json(['data' => new ProjectResource($project)], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            //TODO: add metrics and logs
+            return response()->json([
+                'error' => config('app.debug') ? $e->getMessage() : null
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::prefix('projects')->controller(ProjectController::class)->group(function () {
         Route::get('/', 'index');
+        Route::post('/', 'store');
     });
 
 });
