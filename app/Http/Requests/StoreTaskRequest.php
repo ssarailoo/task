ce6e6ab45aac64c822886fbf8b314b572e7d0e60
ssarailoo <ssarailoo@yahoo.com>
2025-11-10 00:07:42 +0330

    feat: add form requests for task

---
+++ app/Http/Requests/StoreTaskRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TaskStatusEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'project_id' => ['required', 'exists:projects,id'],
+            'status' => ['nullable', 'string',Rule::in(TaskStatusEnum::getValues())],
+            'due_date' => ['nullable', 'date'],
+            'estimated_time' => ['nullable', 'integer', 'min:0'],
+            'actual_time' => ['nullable', 'integer', 'min:0'],
+            'assigned_users' => ['nullable', 'array'],
+            'assigned_users.*' => ['integer', 'exists:users,id'],
+            'dependencies' => ['nullable', 'array'],
+            'dependencies.*' => ['integer', 'exists:tasks,id'],
+        ];
+    }
+}
