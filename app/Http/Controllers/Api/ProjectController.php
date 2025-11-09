3b5ab947de364911721fe6397b4a620e479ff6d9
ssarailoo <ssarailoo@yahoo.com>
2025-11-09 21:20:55 +0330

    feat: add index method to project controller with resource response

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Services\ProjectService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
+    public function __construct(
+        private readonly ProjectService $projectService
+    ) {}
+
+    public function index(Request $request): JsonResponse
+    {
+        $projects = $this->projectService->getAllProjects($request->all());
+
+        return response()->json([
+            'success' => true,
+            'data' => ProjectResource::collection($projects),
+            'meta' => [
+                'current_page' => $projects->currentPage(),
+                'last_page' => $projects->lastPage(),
+                'per_page' => $projects->perPage(),
+                'total' => $projects->total(),
+            ]
+        ]);
+    }
     /**
      * Store a newly created resource in storage.
      */
