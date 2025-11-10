b988fa41e6dee68a38a35cb504bed2af148a5b2a
Saeed <ssarailoo@yahoo.com>
2025-11-10 09:31:26 +0330

    feat: add comment service

---
+++ app/Services/CommentService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\DataTransferObjects\CommentDTO;
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Builder;
+
+readonly class CommentService
+{
+    public function createComment(CommentDTO $data): Comment
+    {
+        return $this->query()->create($data->toArray());
+    }
+
+    public function createReply(Comment $parentComment, CommentDTO $data): Comment
+    {
+        $replyData = array_merge($data->toArray(), [
+            'parent_id' => $parentComment->id,
+            'task_id' => $parentComment->task_id,
+            'rating' => null,
+        ]);
+
+        return $this->query()->create($replyData);
+    }
+
+    public function updateComment(Comment $comment, array $data): Comment
+    {
+        $comment->update($data);
+        return $comment->fresh();
+    }
+
+    public function deleteComment(Comment $comment): bool
+    {
+        return $comment->delete();
+    }
+
+    private function query(): Builder
+    {
+        return Comment::query();
+    }
+}
