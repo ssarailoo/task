54563889c81086f87c845a764893b8ec39e9e151
Saeed <ssarailoo@yahoo.com>
2025-11-10 09:32:53 +0330

    feat: add comment resource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'task_id' => $this->task_id,
+            'user_id' => $this->user_id,
+            'parent_id' => $this->parent_id,
+            'content' => $this->content,
+            'rating' => $this->rating,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'user' => $this->whenLoaded('user'),
+            'replies' => CommentResource::collection($this->whenLoaded('replies')),
+        ];
+    }
+}
